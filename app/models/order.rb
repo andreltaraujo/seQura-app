a78c513ee1e4759bf4a75a8f072740b3779718a6
André Araujo <andreguia@hotmail.com>
2023-10-13 20:44:07 -0300

    - Models - Disbursements 	Add Relations 	Add Validations 	Add Monetize to monetary fields 	Implement generate_reference #methodo 	Implement callbak - Merchant 	Add Relations 	Add Validations 	Add Monetize to monetary fields 	Implement scopes - OrderCommission 	Add Relations 	Add Validations 	Add Monetize to monetary fields - Order 	Implement fee constantes 	Add Relations 	Add Validations 	Add Monetize to monetary fields 	Implement scopes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,26 @@
 class Order < ApplicationRecord
   belongs_to :disbursement
   belongs_to :merchant
+	VALUE_FOR_FEE_100 = 4999
+	VALUES_FOR_FEE_095 = (5000..29999)
+	VALUE_FOR_FEE_085 = 30000
+	
+	belongs_to :disbursement, optional: true
+	belongs_to :merchant
+	has_one :order_commission, dependent: :destroy
+	
+	validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+	validates :merchant_reference, presence: true	
+	
+	monetize :amount_cents
+	
+	scope :for_merchant, ->(merchant_reference) { where(merchant_reference: merchant_reference) }
+	scope :daily_disbursements, ->(merchant_reference, date) { for_merchant(merchant_reference).where(created_at: date.beginning_of_day..date.end_of_day) }
+  scope :within_date_range, ->(start_date, end_date) { where(created_at: start_date.beginning_of_day..end_date.end_of_day) }
+	scope :not_disbursed, -> { where(disbursement_id: nil, disbursement_reference: nil) }
+  scope :weekly_disbursements, ->(merchant_reference, start_date, end_date) {
+		for_merchant(merchant_reference)
+		.within_date_range(start_date, end_date)
+		.not_disbursed
+	}
 end
--- app/models/order_commission.rb
+++ app/models/order_commission.rb
@@ -1,4 +1,10 @@
 class OrderCommission < ApplicationRecord
   belongs_to :order
   belongs_to :disbursement
+	
+	validates :order_amount_cents, :sequra_amount_cents, :merchant_amount_cents, :fee_percentage, :order_date, :order_id, presence: true
+
+	monetize :order_amount_cents
+	monetize :sequra_amount_cents
+	monetize :merchant_amount_cents
 end
