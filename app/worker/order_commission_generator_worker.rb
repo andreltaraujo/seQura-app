a4d3ed24067079cf0e074956ee3f1dff1777051f
André Araujo <andreguia@hotmail.com>
2023-10-13 21:29:37 -0300

    - Workers - Change initialize to receive object and pass object intead of object IDs

--- app/worker/order_commission_generator_worker.rb
+++ app/worker/order_commission_generator_worker.rb
@@ -1,9 +1,9 @@
 class OrderCommissionGeneratorWorker
   include Sidekiq::Worker
 
-  def perform(order_id)
+  def perform(order)
 		Rails.logger.info("[#{self.class.name}][Starting generation...]")
-    OrderCommissionGenerator.new.execute(order_id)
+    OrderCommissionGenerator.new.execute(order)
     Rails.logger.info("[#{self.class.name}][Generation finished. ] #{Time.now.utc.to_s}")
   rescue StandardError => e
     Rails.logger.error("[#{self.class.name}][Error] #{e.message}")
