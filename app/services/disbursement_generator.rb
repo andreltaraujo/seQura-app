16928f8ec9893ca50830cea4df13fe3792b6a623
André Araujo <andreguia@hotmail.com>
2023-10-14 08:34:48 -0300

    - Services - Implement daily generation disbursements

--- app/services/disbursement_generator.rb
+++ app/services/disbursement_generator.rb
@@ -1,47 +1,83 @@
 class DisbursementGenerator
 
 	def execute
-		merchants_to_process = DisbursementGenerator.merchants_weekly_today
-		merchants_to_process.each do |merchant|
-			dates = DisbursementGenerator.weekly_disbursements_dates(merchant)
-			orders_to_process = DisbursementGenerator.fetch_orders_weekly(merchant, dates)
-			if orders_to_process.any?
-				generated_commissions = OrderCommissionGenerator.new.execute(orders_to_process)
-				if generated_commissions.present?
-					calculated_disbursement = DisbursementCalculator.new(generated_commissions).calculate
+		daily_merchants_to_process = merchants_daily
+		weekly_merchants_to_process = merchants_weekly_today
+		daily_merchants_to_process.each do |daily_merchant|
+			day_date = Date.yesterday
+			day_orders_to_process = fetch_day_orders(daily_merchant, day_date)
+			if day_orders_to_process.any?
+				generated_day_commissions = OrderCommissionGenerator.new.execute(day_orders_to_process)
+				if generated_day_commissions.present?
+					calculated_day_disbursement = DisbursementCalculator.new(generated_day_commissions).calculate
 					begin
 						Disbursement.transaction do
-							created_disbursement = Disbursement.create(calculated_disbursement)
+							created_day_disbursement = Disbursement.create(calculated_day_disbursement)
+							updated_disbursed_day_commissions = []
+							day_orders_to_process.each do |day_commission|
+								day_commission.update(disbursement_id: created_disbursement.id)
+								updated_disbursed_day_commissions << day_commission.id
+								order = day_commission.order
+								order.update(disbursement_id: created_day_disbursement.id, disbursement_reference: created_day_disbursement.reference)
+								updated_disbursed_day_commissions << order.id
+								Rails.logger.info "Day Disbursement created for #{updated_disbursed_day_commissions} for daily merchant #{daily_merchant.reference}"
+							end
+						end
+					rescue ActiveRecord::RecordInvalid => invalid
+						Rails.logger.error "[DisbursementsDailyTask][Validation failed] #{invalid.message} #{invalid.backtrace}"
+						Rails.logger.info "Day Disbursement created for #{updated_disbursed_day_commissions} for daily merchant #{daily_merchant.reference}"
+					rescue StandardError => e
+						Rails.logger.error "[DisbursementsDailyTask][error] #{e.message} #{e.backtrace}"
+					end
+				else
+					Rails.logger.info "[DisbursementsDailyTask][Order commissions wasn't generated] for daily merchant #{daily_merchant.reference}"
+				end
+			else
+				Rails.logger.info "[DisbursementsDailyTask][No order commissions to process] for daily merchant #{daily_merchant.reference}"
+			end
+		end
+
+		weekly_merchants_to_process.each do |weekly_merchant|
+			week_dates = week_disbursements_dates(weekly_merchant)
+			week_orders_to_process = fetch_week_orders(weekly_merchant, week_dates)
+			if week_orders_to_process.any?
+				debugger
+				generated_week_commissions = OrderCommissionGenerator.new.execute(week_orders_to_process)
+				if generated_week_commissions.present?
+					calculated_week_disbursement = DisbursementCalculator.new(generated_week_commissions).calculate
+					begin
+						Disbursement.transaction do
+							created_week_disbursement = Disbursement.create(calculated_week_disbursement)
 							updated_disbursed_week_commissions = []
-							orders_to_process.each do |commission|
-								commission.update(disbursement_id: created_disbursement.id)
+							week_orders_to_process.each do |commission|
+								commission.update(disbursement_id: created_week_disbursement.id)
 								updated_disbursed_week_commissions << commission.id
 								order = commission.order
-								order.update(disbursement_id: created_disbursement.id, disbursement_reference: created_disbursement.reference)
+								order.update(disbursement_id: created_week_disbursement.id, disbursement_reference: created_week_disbursement.reference)
 								updated_disbursed_week_commissions << order.id
-								Rails.logger.info "Disbursement created for #{updated_disbursed_week_commissions} for merchant #{merchant.reference}"
+								Rails.logger.info "Disbursement created for #{updated_disbursed_week_commissions} for weekly merchant #{weekly_merchant.reference}"
 							end
 						end
 					rescue ActiveRecord::RecordInvalid => invalid
 						Rails.logger.error "[DisbursementsWeeklyTask][Validation failed] #{invalid.message} #{invalid.backtrace}"
-						Rails.logger.info "Disbursement created for #{updated_disbursed_commissions} for merchant #{merchant.reference}"
+						Rails.logger.info "Week Disbursement created for #{updated_disbursed_week_commissions} for weekly merchant #{weekly_merchant.reference}"
 					rescue StandardError => e
 						Rails.logger.error "[DisbursementsWeeklyTask][error] #{e.message} #{e.backtrace}"
 					end
 				else
-					Rails.logger.info "[DisbursementsWeeklyTask][Order commissions wasn't generated] for merchant #{merchant.reference}"
+					Rails.logger.info "[DisbursementsWeeklyTask][Order commissions wasn't generated] for weekly merchant #{weekly_merchant.reference}"
 				end
 			else
-				Rails.logger.info "[DisbursementsWeeklyTask][No order commissions to process] for merchant #{merchant.reference}"
+				Rails.logger.info "[DisbursementsWeeklyTask][No order commissions to process] for weekly merchant #{weekly_merchant.reference}"
 			end
 		end
 	end
 
-	def self.weekly_disbursements_dates(merchant)
+	def week_disbursements_dates(weekly_merchant)
 		current_date = Date.today
 		current_week_day = Date.today.wday
-		if merchant.live_on.wday == current_week_day # Make sure the eligible merchant live_on day is the same as today
-			dates = { 
+		if weekly_merchant.live_on.wday == current_week_day # Make sure the eligible weekly merchant live_on day is the same as today, even it was checked and selected before
+			week_dates = { 
 				merchant_week_start_date: current_date - 6.days,
 				merchant_week_end_date: current_date - 1.day
 			}		
@@ -51,18 +87,18 @@ class DisbursementGenerator
 		end
 	end
 	
-	def self.merchants_daily
+	def merchants_daily
 		Merchant.daily_eligible_merchants
 	end
 
-	def self.merchants_weekly_today
+	def merchants_weekly_today
 		Merchant.weekly_eligible_merchants		
 	end
-	def self.fetch_orders_daily(merchant, date)
-		Order.daily_disbursements(merchant.reference, date)
+	def fetch_day_orders(daily_merchant, day_date)
+		Order.daily_disbursements(merchant.reference, day_date)
 	end
 
-	def self.fetch_orders_weekly(merchant, dates)
-		Order.weekly_disbursements(merchant.reference, dates[:merchant_week_start_date], dates[:merchant_week_end_date])
+	def fetch_week_orders(weekly_merchant, week_dates)
+		Order.weekly_disbursements(merchant.reference, week_dates[:merchant_week_start_date], dates[:merchant_week_end_date])
 	end
 end
